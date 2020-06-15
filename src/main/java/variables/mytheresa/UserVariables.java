d4a39d4b421bf46704d71f5693fc4eccb227d0c3
shireeshamonduti <39485744+shireeshamonduti@users.noreply.github.com>
2020-06-15 11:22:39 +0530

    Update UserVariables.java

--- src/main/java/variables/mytheresa/UserVariables.java
+++ src/main/java/variables/mytheresa/UserVariables.java
@@ -1,9 +1,9 @@
 package variables.mytheresa;
 
 public final class UserVariables {
-	public static final String USER_FIRST_NAME = "First Name3";
-	public static final String USER_LAST_NAME = "Last Name3";
-	public static final String USER_EMAIL = "example3@gmail.com";
+	public static final String USER_FIRST_NAME = "First Name31";
+	public static final String USER_LAST_NAME = "Last Name31";
+	public static final String USER_EMAIL = "example31@gmail.com";
 	public static final String USER_PASSWORD = "123456pass";
 	public static final String USER_NEW_PASSWORD = "pass123456";
 	
